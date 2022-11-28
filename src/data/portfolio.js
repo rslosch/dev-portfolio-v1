0db3a0de0af17c48e78f9f39fd04e26bf3923e4e
Steven <slosch99@gmail.com>
2022-11-28 17:03:16 -0500

    Portfolio card component and add portfolio data file

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Quote Entry App',
+        imgUrl: '/assets/myqoutes-app.png',
+        stack: ['HTML', 'CSS', 'Javascript', 'React', 'Ruby on Rails'],
+        link: 'TBD'
+    },
+    {
+        title: 'FullStack App #2',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'Javascript', 'React', 'Ruby on Rails'],
+        link: 'TBD'
+    },
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['React', 'TailWindCSS', 'Vite'],
+        link: 'TBD'
+    }
+]
