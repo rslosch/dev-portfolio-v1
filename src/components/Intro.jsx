6a535ec88bc14b1a5c94758b93b959c6fc01ebeb
Steven <slosch99@gmail.com>
2022-11-29 10:24:45 -0500

    Implement dark mode

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Steven Losch</h1>
+        <h1 className='text-4xl dark:text-white md:text-7xl mb-1 md:mb-3 font-bold'>Steven Losch</h1>
         <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer & Web Developer</p>
         <p className='text-sm max-w-xl mb-6 font-bold'>
             I'm a passionate and curious learner, former Accountant and now Software Engineer looking forward to starting a new career.
