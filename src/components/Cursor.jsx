f64dcaf125fa1e912fa11271dcfae65702a6fac1
Steven <slosch99@gmail.com>
2023-02-27 21:01:49 -0600

    Add mouseclick listener for custom cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -11,6 +11,7 @@ const Cursor = () => {
 		x:0,
 		y:0
 	})
+    const [isClicked, setIsClicked] = useState(false)
 	  
 	  useEffect(() => {
 		const mouseMove = (e) => {
@@ -23,19 +24,29 @@ const Cursor = () => {
             y: e.clientY
           })
 		}
+
+        const handleClick = () => {
+            setIsClicked(true)
+            setTimeout(() => {
+              setIsClicked(false)
+            }, 300)
+          }      
 	  
 		window.addEventListener("mousemove", mouseMove)
+        window.addEventListener("click", handleClick)
 	  
 		return () => {
 		  window.removeEventListener("mousemove", mouseMove)
+            window.removeEventListener("click", handleClick)
 		}
 	  })
 	  
 	  const cursorLarge = {
 		default: {
-		  x: mousePositionLarge.x -16,
-		  y: mousePositionLarge.y -16,
-		  transition: {type:"spring", dampness:5}
+            scale: isClicked ? 2 : 1,
+            x: mousePositionLarge.x -16,
+            y: mousePositionLarge.y -16,
+            transition: {type:"spring", dampness:10, scale:{duration: 0.15, ease: "easeInOut"}}
 		}
 	  }
 
@@ -43,13 +54,14 @@ const Cursor = () => {
         default: {
             x: mousePositionSmall.x - 4,
             y: mousePositionSmall.y - 4,
+            transition: {duration: 0.05}
         }
       }
 
   return (
     <div>
-        <motion.div variants={cursorLarge} animate="default" className='pointer-events-none fixed border-2 border-black h-8 w-8 rounded-full z-[998]'></motion.div>
-        <motion.div variants={cursorSmall} animate="default" className="pointer-events-none fixed bg-primary-3 h-2 w-2 rounded-full z-[999]"></motion.div>
+        <motion.div variants={cursorLarge} animate="default" className={`pointer-events-none fixed border-2 ${isClicked ? "border-double" : "border-dotted"} border-primary-3 h-8 w-8 rounded-full z-[501]`}></motion.div>
+        <motion.div variants={cursorSmall} animate="default" className={`pointer-events-none fixed bg-primary-3 h-2 w-2 rounded-full z-[500]`}></motion.div>
     </div>
     )
 }
