ea6c9b0108fd5746755e884f924e63561b8b3708
Steven <slosch99@gmail.com>
2023-03-02 09:54:49 -0600

    Change portfolio data

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,23 @@
 export default [
     {
-        title: 'Quote Entry App',
-        imgUrl: '/assets/myqoutes-app.png',
-        stack: ['HTML', 'CSS', 'Javascript', 'React', 'Ruby on Rails'],
-        link: 'TBD'
+        title: 'Cards For Insanity',
+        description: "Magna qui adipisicing veniam aliquip id duis ipsum. Culpa sint nulla excepteur deserunt veniam exercitation quis. Laborum dolore anim minim ex reprehenderit occaecat aute aliqua laborum officia qui laboris tempor aliqua. Sint nisi enim sunt ipsum mollit officia do ut ullamco irure adipisicing Lorem velit. Voluptate pariatur veniam cillum dolore fugiat aliqua consectetur occaecat commodo.",
+        stack: ['HTML', 'TailwindCSS', 'Framer Motion', 'Javascript', 'React', 'Ruby on Rails'],
+        githubLink: 'https://github.com/rslosch/card-game',
+        websiteLink: 'https://cards-for-insanity-app.onrender.com/'
     },
     {
-        title: 'Cards Against Insanity',
-        imgUrl: '/assets/cards-for-insanity-app.png',
-        stack: ['HTML', 'TailwindCSS', 'Framer Motion', 'Javascript', 'React', 'Ruby on Rails'],
-        link: 'TBD'
+        title: 'MyQuotes App',
+        description: "Incididunt sunt nulla ut commodo. Commodo eiusmod consectetur esse ex ex culpa minim pariatur elit. Minim laboris enim ex aliqua irure non adipisicing. Voluptate Lorem officia qui deserunt velit occaecat Lorem consequat consectetur. Occaecat adipisicing aliquip amet ad cupidatat consequat proident laboris dolore Lorem id deserunt ad.",
+        stack: ['HTML', 'CSS', 'Javascript', 'React', 'Ruby on Rails'],
+        githubLink: 'https://github.com/rslosch/MyQuotes-project',
+        websiteLink: 'https://myquotes-app.onrender.com/'
     },
     {
         title: 'JobKanban',
-        imgUrl: '/assets/jobkanban-app.png',
+        description: "Deserunt voluptate laborum eiusmod sit aliqua non sint esse. Deserunt ut enim reprehenderit culpa laboris proident quis enim eiusmod Lorem exercitation elit laborum. Velit do nostrud culpa eiusmod consectetur do in. Lorem eiusmod minim nisi anim. Fugiat ut sit nulla aliquip veniam veniam non est deserunt sint deserunt aute.",
         stack: ['React', 'TailWindCSS', 'React dndKit','Javascript', 'Ruby on Rails'],
-        link: 'TBD'
+        githubLink: 'https://github.com/rslosch/jobkanban-app',
+        websiteLink: 'TBD'
     }
 ]
